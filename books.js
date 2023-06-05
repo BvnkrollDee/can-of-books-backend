82b2ac778cda2725aba4993d155077747d4eebb8
Daron Faulkner Jr <dj200099@gmail.com>
2023-06-05 11:39:43 -0500

    changes resloved

--- books.js
+++ books.js
@@ -8,5 +8,12 @@ const bookSchema = new mongoose.Schema({ // Define the schema for the book colle
 
 const bookModel = mongoose.model('book', bookSchema); // Create a Mongoose model for the book collection
 
+let books = [ // Create an array of book objects for testing purposes
+    {title: 'test', description: 'test', status: 'test'},
+    {title: 'test2', description: 'test2', status: 'test2'},
+    {title: 'test3', description: 'test3', status: 'test3'}
+];
+
+bookModel.insertMany(books); // Insert the book objects into the book collection in the database
 
 module.exports = bookModel; // Export the bookModel as a module
