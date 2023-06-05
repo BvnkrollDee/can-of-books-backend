d95b1f1169305c066ba409785339ec7cf385fb48
CEEJ987 <123979158+CEEJ987@users.noreply.github.com>
2023-06-05 13:57:29 -0500

    lab 12 branch

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const books = require('./books')
-
 const mongoose = require('mongoose');
 const seed = require('./seed');
 const app = express();
